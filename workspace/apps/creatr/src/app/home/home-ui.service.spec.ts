82e501e72dc42543782ceeafa05c06f83a7b854f
Matt Vaughn` <mvaughn@herffjones.com>
2020-12-18 12:30:42 -0700

    update tests

--- workspace/apps/creatr/src/app/home/home-ui.service.spec.ts
+++ workspace/apps/creatr/src/app/home/home-ui.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { LoggingService, LoggingServiceMock } from '@valencia/logging';
 
-import { HomeUIService } from './home.service';
+import { HomeUIService } from './home-ui.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('HomeUIService', () => {
   let service: HomeUIService;
 
+  /**
+   * Provide the [service] that is tested to the [Testing Module].
+   * A reference is required so that in can be injected in line 23
+   * and available during the actual tests.
+   */
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // TestBed --> Module (container)
+    TestBed.configureTestingModule({
+      providers: [
+        HomeUIService, // 1. provide this targe service
+        {
+          provide: LoggingService, // 2. included in ctor of service
+          useClass: LoggingServiceMock,
+        },
+      ],
+    });
     service = TestBed.inject(HomeUIService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have a service name', () => {
+    expect(service.serviceName).toBeTruthy();
   });
 });
